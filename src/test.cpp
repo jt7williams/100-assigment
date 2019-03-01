5ffd33c7946c7a06bdf97eb673dd714a35a8f2b0
William Montes <jt7williams@aol.com>
2019-03-01 10:12:03 -0800

    changed test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -7,7 +7,16 @@ test::test() {
 test::test(string n) {
 
 }
+bool test::compute(bool* flag){
+
+
+}
+bool test::set_command(string n){
+
+	std::cout<<n<<std::end;
+}
 
 int test::type() {
 	return 8;
 }
+
